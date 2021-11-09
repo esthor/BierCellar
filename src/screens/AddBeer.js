20da27439467468711b0546ea899cad6bf5ddaaf
Erik Thorelli <ethorelli@gmail.com>
2021-11-09 18:41:06 -0500

    session 3 - cleanup addbeer screen

--- src/screens/AddBeer.js
+++ src/screens/AddBeer.js
@@ -1,44 +1,71 @@
 import React, {useState} from 'react';
-import {Alert, StyleSheet, Text, TextInput, View} from 'react-native';
-import {brandColor, lightPink, screenBackgroundColor} from '../style/colors';
-import {bigBigText} from '../style/typography';
+import {
+  Alert,
+  StyleSheet,
+  ScrollView,
+  Text,
+  TextInput,
+  useWindowDimensions,
+  View,
+} from 'react-native';
+import {
+  brandColor,
+  gray,
+  lightPink,
+  screenBackgroundColor,
+} from '../style/colors';
+import {bigBigText, inputTextSize} from '../style/typography';
 
 import BCText from '../components/BCText';
 import BCButton from '../components/BCButton';
 
-const AddBeerScreen = () => {
+const AddBeerForm = () => {
   const [breweryName, setBreweryName] = useState('Brewery Name');
   const [beerName, setBeerName] = useState('Beer Name');
-  const [year, setYear] = useState('Year');
-  const [price, setPrice] = useState('Price');
-  const [notes, setNotes] = useState('Notes');
+  const {height, width} = useWindowDimensions();
+
   return (
     <View style={styles.container}>
       <BCText textStyle="header" value="Please add a beer" />
-      <TextInput onChangeText={setBreweryName} value={breweryName} />
-      <TextInput onChangeText={setBeerName} value={beerName} />
-      <TextInput onChangeText={setYear} value={year} keyboardType="numeric" />
-      <TextInput
-        onChangeText={setPrice}
-        value={price}
-        placeholder="useless placeholder"
-        keyboardType="numeric"
-      />
-      <TextInput onChangeText={setNotes} value={notes} />
-      <BCButton
-        buttonStyle="submit"
-        onPress={() =>
-          console.log(
-            `You have successfully logged a ${year} ${breweryName} ${beerName} `,
-          )
-        }>
-        <BCText textStyle="buttonText" value="Add Beer" />
-      </BCButton>
+      <ScrollView style={[styles.inputFieldsContainer, {width: width}]}>
+        <TextInput
+          onChangeText={setBreweryName}
+          style={styles.input}
+          value={breweryName}
+        />
+        <TextInput
+          onChangeText={setBeerName}
+          style={styles.input}
+          value={beerName}
+        />
+      </ScrollView>
+      <View style={styles.buttonContainer}>
+        <BCButton
+          buttonStyle="submit"
+          onPress={() =>
+            console.log(
+              `You have successfully logged a ${breweryName} ${beerName} `,
+            )
+          }>
+          <BCText textStyle="buttonText" value="Add Beer" />
+        </BCButton>
+      </View>
+    </View>
+  );
+};
+
+const AddBeerScreen = () => {
+  return (
+    <View style={styles.container}>
+      <AddBeerForm />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  buttonContainer: {
+    margin: 10,
+  },
   container: {
     flex: 1,
     backgroundColor: screenBackgroundColor,
@@ -48,6 +75,20 @@ const styles = StyleSheet.create({
   helloText: {
     color: brandColor,
     fontSize: bigBigText,
+  },
+  inputFieldsContainer: {
+    flex: 1,
+    width: 100,
+  },
+  input: {
+    color: gray,
+    height: 40, // TODO: make this a constant, not a literal
+    margin: 12, // TODO: make this a constant, not a literal
+    borderWidth: 1, // TODO: make this a constant, not a literal
+    borderColor: gray,
+    borderRadius: 12, // TODO: make this a constant, not a literal
+    padding: 10, // TODO: make this a constant, not a literal
+    fontSize: inputTextSize,
   },
 });
 
--- src/style/typography.js
+++ src/style/typography.js
@@ -1,6 +1,7 @@
 export const bigBigText = 98;
 export const headerSize = 40;
 export const buttonTextSize = 20;
+export const inputTextSize = 20;
 
 export const navigationStyles = {
   headerFontSize: 25,
